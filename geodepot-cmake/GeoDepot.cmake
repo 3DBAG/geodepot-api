9dc85ed80a1c89e47ce8f67ac252486cd68c85c4
Balázs Dukai <balazs.dukai@gmail.com>
2024-09-18 13:15:37 +0200

    CMake module

--- geodepot-cmake/GeoDepot.cmake
+++ geodepot-cmake/GeoDepot.cmake
@@ -2,13 +2,13 @@ include(FetchContent)
 FetchContent_Declare(
   geodepot-api
   GIT_REPOSITORY https://github.com/3DGI/geodepot-api.git
-  GIT_TAG        0667404ff96306e26e4e6f4f3c17fd24b4d8575d
+  GIT_TAG        56de51fd64e29ba4f257fa582113540a8cc9649e
 )
 FetchContent_MakeAvailable(geodepot-api)
 
 try_compile(GEODEPOT_INIT_COMPILE_RESULTS
         PROJECT geodepot-api
-        BINARY_DIR ${CMAKE_BINARY_DIR}/geodepot-api-build
+        BINARY_DIR "${CMAKE_BINARY_DIR}/geodepot-api-build"
         SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/geodepot-api-src"
         TARGET geodepot-init
         CMAKE_FLAGS -DGD_BUILD_TESTING=OFF -DGD_BUILD_BINDINGS=OFF -DGD_BUILD_APPS=ON
@@ -16,16 +16,15 @@ try_compile(GEODEPOT_INIT_COMPILE_RESULTS
 
 try_compile(GEODEPOT_INIT_COMPILE_RESULTS
         PROJECT geodepot-api
-        BINARY_DIR ${CMAKE_BINARY_DIR}/geodepot-api-build
+        BINARY_DIR "${CMAKE_BINARY_DIR}/geodepot-api-build"
         SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/geodepot-api-src"
         TARGET geodepot-get
         CMAKE_FLAGS -DGD_BUILD_TESTING=OFF -DGD_BUILD_BINDINGS=OFF -DGD_BUILD_APPS=ON
 )
 
 function(GeodepotInit URL)
-    message(${URL})
     execute_process(
-            COMMAND ${CMAKE_BINARY_DIR}/geodepot-api-build/apps/init/geodepot-init ${URL}
+            COMMAND "${CMAKE_BINARY_DIR}/geodepot-api-build/apps/init/geodepot-init" ${URL}
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             COMMAND_ERROR_IS_FATAL ANY
     )
@@ -33,9 +32,8 @@ function(GeodepotInit URL)
 endfunction()
 
 function(GeodepotGet CASESPEC)
-    message(${CASESPEC})
     execute_process(
-            COMMAND ${CMAKE_BINARY_DIR}/geodepot-api-build/apps/get/geodepot-get ${CASESPEC}
+            COMMAND "${CMAKE_BINARY_DIR}/geodepot-api-build/apps/get/geodepot-get" ${CASESPEC}
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             COMMAND_ERROR_IS_FATAL ANY
     )
