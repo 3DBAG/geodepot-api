6a20392f5a3ed58e5762eae4e08efb1dbff59b47
Balázs Dukai <balazs.dukai@gmail.com>
2026-09-09 21:53:19 +0200

    test: add geodepot remote integration suite

--- include/geodepot/geodepot.h
+++ include/geodepot/geodepot.h
@@ -1,67 +1,10 @@
-// Copyright (c) 2024 Balázs Dukai (3DGI), Ravi Peters (3DGI)
-//
-// This file is part of geodepot (https://github.com/3DGI/geodepot)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// Author(s):
-// Balázs Dukai
-
 #pragma once
 #include <filesystem>
-#include <optional>
-#include <string_view>
-
-#if defined(_WIN32)
-#  if defined(EXPORTING_MYMATH)
-#    define DECLSPEC __declspec(dllexport)
-#  else
-#    define DECLSPEC __declspec(dllimport)
-#  endif
-#else // non windows
-#  define DECLSPEC
-#endif
-
+#include <stdexcept>
+#include <string>
 namespace geodepot {
-
-  struct DECLSPEC CaseSpec {
-    std::string case_name;
-    std::string data_name;
-
-    static CaseSpec from_string(std::string casespec_string);
-    [[nodiscard]] std::filesystem::path to_path() const;
-  };
-
-  class DECLSPEC Repository {
-   public:
-    explicit Repository(std::string_view path);
-    Repository() = default;
-
-    [[nodiscard]] std::optional<std::filesystem::path> get(
-        std::string casespec) const;
-    [[nodiscard]] std::filesystem::path get_repository_path() const;
-
-   private:
-    std::filesystem::path path_;
-    std::filesystem::path path_cases_;
-    std::filesystem::path path_index_;
-    std::filesystem::path path_config_local_;
-
-    bool is_valid() const;
-  };
-
-  bool DECLSPEC is_url(std::string_view path);
-
-  bool DECLSPEC download(std::string url, std::string dest);
-
+struct PrepareOptions { std::filesystem::path lock_file; std::filesystem::path cache_root; std::string suite; bool offline = false; };
+enum class ErrorCode { cli=2, missing=3, network=4, integrity=5, metadata=6, cache=7 };
+class Error final : public std::runtime_error { public: Error(ErrorCode c, std::string m): std::runtime_error(std::move(m)), code_(c) {} [[nodiscard]] ErrorCode code() const noexcept { return code_; } private: ErrorCode code_; };
+std::filesystem::path prepare(const PrepareOptions& options);
 }
--- justfile
+++ justfile
@@ -2,3 +2,7 @@ download-data:
     wget https://data.3dgi.xyz/geodepot-test-data/data.zip
     unzip -o data.zip
     rm data.zip
+
+test-integration:
+    cmake -S . -B build/integration -DGD_BUILD_TESTING=ON -DGD_BUILD_APPS=ON
+    cmake --build build/integration --target test-integration
